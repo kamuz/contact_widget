661fb87f28549b90a5982e9f0bd696910182c646
Vladimir Kamuz <v.kamuz@gmail.com>
2018-12-21 07:29:28 +0200

    Include class, register widget and check script

--- contact-widget.php
+++ contact-widget.php
@@ -7,3 +7,24 @@ Author: Vladimir Kamuz
 Author URI: https://wpdev.pp.ua
 License: GPL2
 */
+
+/**
+ * Include JavaScript
+ */
+function add_scripts(){
+    wp_enqueue_script('contact-scripts', plugins_url(). '/contact-widget/js/script.js', array('jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'add_scripts');
+
+/**
+ * Include Class
+ */
+include('class.contact-widget.php');
+
+/**
+ * Register Widget
+ */
+function register_contact_widget(){
+    register_widget('Contact_Widget');
+}
+add_action('widget_init', 'register_contact_widget');
