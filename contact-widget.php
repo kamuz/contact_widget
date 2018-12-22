9b7c2ffc4112110494dd9d6c523f06467e00782d
Vladimir Kamuz <v.kamuz@gmail.com>
2018-12-22 02:34:57 +0200

    PHP mailer and CSS

--- contact-widget.php
+++ contact-widget.php
@@ -13,6 +13,7 @@ License: GPL2
  */
 function add_scripts(){
     wp_enqueue_script('contact-scripts', plugins_url(). '/contact-widget/js/script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style('contact-style', plugins_url(). '/contact-widget/css/style.css', null, '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'add_scripts');
 
--- mailer.php
+++ mailer.php
@@ -1,2 +1,41 @@
 <?php
-echo $_POST['name'];
+// Check $_POST
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    // Get and sanitize $_POST values
+    $name = strip_tags(trim($_POST['name']));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $message = strip_tags(trim($_POST['message']));
+    $recipient = $_POST['recipient'];
+    $subject = $_POST['subject'];
+
+    // Simple validation
+    if(empty($name) || empty($email) || empty($message)){
+        // Set a 400 (bad request) response code and exit
+        http_response_code(400);
+        echo "Please check your form fields.";
+        exit;
+    }
+
+    // Build Message
+    $text = "Name: $name\n";
+    $text .= "Email: $email\n";
+    $text .= "Message: $message\n";
+
+    // Build Headers
+    $headers = "From: $name <$email>";
+
+    // Send Email
+    if(mail($recipient, $subject, $text, $headers)){
+        // Set 200 response (success)
+        http_response_code(200);
+        echo "Thank You: Your message has been send";
+    } else{
+        // Set 500 response (internal server message)
+        http_response_code(500);
+        echo "Error: There was a problem sending your message";
+    }
+} else{
+    // Set 403 response (forbidden)
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
